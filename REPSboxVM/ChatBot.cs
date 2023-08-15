7d246a65fa9c3562cd0d7f34358046c5ee463164
Alessandro Proto <alex@alexdevs.me>
2023-08-15 13:48:07 +0200

    Bugfix command being lowercased in `\=` mode

--- REPSboxVM/ChatBot.cs
+++ REPSboxVM/ChatBot.cs
@@ -67,7 +67,7 @@ internal class ChatBot
         if (doFunny)
         {
             var singleArg = string.Join(' ', ev.Args);
-            var fullComand = cmd + singleArg;
+            var fullComand = ev.Command + singleArg;
             if (fullComand.StartsWith('='))
             {
                 ev.Args = fullComand[1..].Split(' ');
